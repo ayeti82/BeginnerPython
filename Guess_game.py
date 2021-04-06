f1f339964033136c684042409a2db639aa82cb19
Ayetijhya Desmukhya <81956294+ayeti82@users.noreply.github.com>
2021-04-06 06:54:33 +0200

    Update Guess_game.py

--- Guess_game.py
+++ Guess_game.py
@@ -1,11 +1,16 @@
 import random
 
+
+def guess_closeness(g, n):
+    print("Hint:\tWinning number is lower") if g > n else print("Hint:\tWinning number is higher")
+
+
 if __name__ == '__main__':
-    print("Range of the game 1-20\nYou have 9 guesses in total")
-    chances = 9
+    print("Range of the game 1-20\nYou have 5 guesses in total")
+    chances = 5
     num = random.randint(1, 20)
     won = False
-    while chances>0:
+    while chances > 0:
         print("Enter your guess:")
         guess = int(input())
         if guess == num:
@@ -15,7 +20,8 @@ if __name__ == '__main__':
         else:
             chances -= 1
             if chances != 0:
-                print("Try again!\nYou have ", chances,"chances left")
+                guess_closeness(guess, num)
+                print("Try again!\t[Chances:", chances, "]")
 
     if not won:
         print("You lose! The number was", num)
